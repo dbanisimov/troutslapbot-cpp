ef77ee82c094a36014b08eec375f4908bf9d6b9f
Denis Anisimov <dbanisimov@gmail.com>
2015-11-02 23:32:46 +0300

    Add usage message

--- slap.cpp
+++ slap.cpp
@@ -5,10 +5,20 @@
  * 
  */
 
+#include <iostream>
+
 #include "trout_slap_bot.h"
 
 int main(int argc, char** argv) {
-  TroutSlapBot bot("abcd");
-  TroutSlapBot.Run();
+  if (argc < 2) {
+    std::cout << "Usage: slap <bot_token>" << std::endl;
+    exit(1);
+  }
+
+  std::string token(argv[1]);
+  TroutSlapBot bot(token);
+
+  bot.Run();
+
   return 0;
 }
